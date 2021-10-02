4b17097b59b154eb2582a91ce473fd8bd5f7fa83
ababaian <ababaian@bccrc.ca>
2021-10-02 13:35:20 +0100

    revert app

--- app/app.py
+++ app/app.py
@@ -10,36 +10,26 @@ def handler(event, context):
     print('## CONTEXT')
     print(context)
 
-    result_file = analyze_sequence(event['sequence'], event['hash'])
+    result_file = analyze_sequence(event['sequence'])
 
     result_filename = put_report_to_s3(event['hash'], result_file)
 
     return result_filename
 
 
-def analyze_sequence(sequence, fahash):
-    text_file = open("/tmp/submission.fa", "w")
+def analyze_sequence(sequence):
+    text_file = open("/tmp/waxsys.fa", "w")
     text_file.write(sequence)
     text_file.close()
 
-    # Copy input file to S3
-    result_filename = 'input/' + fahash + '.fa'
-    with open("/tmp/submission.fa") as f:
-        string = f.read()
+    subprocess.call(['sh', '/home/palmid/palmid.sh', '-i', '/tmp/waxsys.fa', '-o', 'waxsys', '-d', '/tmp'])
 
-    encoded_string = string.encode("utf-8")
-    s3 = boto3.resource("s3")
-    s3.Bucket('openvirome.com').put_object(Key=result_filename, Body=encoded_string, ContentType='text/html')
-
-    # Run palmID container
-    subprocess.call(['sh', '/home/palmid/palmid.sh', '-i', '/tmp/submission.fa', '-o', 'submission', '-d', '/tmp'])
-
-    result_html = "/tmp/submission.nb.html"
+    result_html = "/tmp/waxsys.nb.html"
     return result_html
 
 
-def put_report_to_s3(fahash, file):
-    result_filename = fahash + '.html'
+def put_report_to_s3(filename, file):
+    result_filename = filename + '.html'
     with open(file) as f:
         string = f.read()
 
